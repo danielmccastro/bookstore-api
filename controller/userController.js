18b798bd750ca002803ce6f859c77ceeba4bfb99
Daniel Castro <danielmccastro@gmail.com>
2025-06-23 20:58:33 -0300

    Created new routes

--- controller/userController.js
+++ controller/userController.js
@@ -27,6 +27,37 @@ module.exports = class UserController {
         }
     }
 
+    static async deleteUser(req, res) {
+        const id_user = req.params.id_user;
+        try {
+            await User.destroy({ where: { id_user } });
+            res.json({ message: 'User deleted successfully' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
+    static async updateUser(req, res) {
+        const id_user_param = parseInt(req.params.id_user);
+        const id_user_token = req.decoded.id_user;
+
+        if (isNaN(id_user_param)) {
+            return res.status(400).json({ error: 'ID inválido' });
+        }
+
+        if (id_user_param !== id_user_token) {
+            return res.status(403).json({ error: 'Você só pode atualizar os seus próprios dados.' });
+        }
+
+        const { name, password, email } = req.body;
+        try {
+            await User.update({ name, password, email }, { where: { id_user: id_user_param } });
+            res.json({ message: 'Dados atualizados com sucesso.' });
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
+    }
+
     static async verifyLogin(req, res) {
         try {
             const { name, password } = req.body;
@@ -45,28 +76,27 @@ module.exports = class UserController {
     }
 
     static async verifyToken(req, res, next) {
-    let token = req.headers['x-access-token'] || req.headers['authorization'];
+        let token = req.headers['x-access-token'] || req.headers['authorization'];
 
-    if (!token) {
-        return res.status(403).json({ error: 'No token provided' });
-    }
-
-    // Remove "Bearer " do início do token, se existir
-    if (token.startsWith('Bearer ')) {
-        token = token.slice(7, token.length);
-    }
-
-    jwt.verify(token, process.env.SECRET, (err, decoded) => {
-        console.log('Token recebido:', token);
-        console.log('Secret usada:', process.env.SECRET);
-
-        if (err) {
-            return res.status(401).json({ error: 'Invalid token' });
+        if (!token) {
+            return res.status(403).json({ error: 'No token provided' });
         }
 
-        req.decoded = decoded;
-        next();
-    });
-}
+        if (token.startsWith('Bearer ')) {
+            token = token.slice(7, token.length);
+        }
+
+        jwt.verify(token, process.env.SECRET, (err, decoded) => {
+            console.log('Token recebido:', token);
+            console.log('Secret usada:', process.env.SECRET);
+
+            if (err) {
+                return res.status(401).json({ error: 'Invalid token' });
+            }
+
+            req.decoded = decoded;
+            next();
+        });
+    }
 
 };
--- routes/routes.js
+++ routes/routes.js
@@ -9,10 +9,14 @@ router.get("/", (req, res) => {
 })
 
 router.post("/add_user", userController.registerUser);
-router.get("/users/:id_user?", userController.getUsers);
+router.put("/update_user/:id_user", userController.verifyToken, userController.updateUser);
+router.post("/delete_user/:id_user", userController.verifyToken, userController.deleteUser);
+router.get("/users/:id_user?", userController.verifyToken, userController.getUsers);
 router.post("/login", userController.verifyLogin);
 
 router.post("/add_book", userController.verifyToken, bookController.registerBook);
+router.put("/update_book/:id_book", userController.verifyToken, bookController.updateBook);
+router.post("/delete_book/:id_book", userController.verifyToken, bookController.deleteBook);
 router.get("/books/:id_book?", bookController.getBooks);
 
 module.exports = router;
